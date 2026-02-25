9c7568115201705a05e84300fac76595011e5890
まめ <marvel.mel0704@gmail.com>
2026-02-25 14:03:08 +0900

    フッター内ルーティンボタンにリンク追加

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,11 @@
 module ApplicationHelper
   def routine_path_for(user)
+    return root_path unless user
+
     if user.parent?
-      "#"
+      parent_role_root_path
     else
-      "#"
+      child_role_routines_path
     end
   end
 end
