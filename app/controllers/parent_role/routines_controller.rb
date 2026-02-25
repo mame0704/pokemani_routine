be37b0b87d4a4b290e2bacf06b34d65b29a50ed2
まめ <marvel.mel0704@gmail.com>
2026-02-25 09:37:53 +0900

    parentのnamespace化による修正

---
+++ app/controllers/parent_role/routines_controller.rb
@@ -0,0 +1,30 @@
+module ParentRole
+  class RoutinesController < ApplicationController
+    layout "parent"
+    before_action :set_child
+
+    def new
+      @routine = @child.routines.build
+    end
+
+    def create
+      @routine = @child.routines.build(routine_params)
+
+      if @routine.save
+        redirect_to parent_role_child_routines_path(@child)
+      else
+        redirect_to new_parent_role_child_routine_path(@child),alert: "ルーティンの作成に失敗しました。"
+      end
+    end
+
+    private
+
+    def set_child
+      @child = current_user.children.find(params[:child_id])
+    end
+
+    def routine_params
+      params.require(:routine).permit(:title, items: [])
+    end
+  end
+end
